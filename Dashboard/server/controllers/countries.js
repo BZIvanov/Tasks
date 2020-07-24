8a6e1bb93d77b76ac2823a0ec0a82768db99e2a4
BZIvanov <ivanov.biser@gmx.com>
2020-07-24 20:28:23 +0300

    setup connection to postgres database

--- Dashboard/server/controllers/countries.js
+++ Dashboard/server/controllers/countries.js
@@ -1,5 +1,12 @@
-exports.summary = (req, res) => {
-  res.status(200).json({
-    success: true,
-  });
+const pool = require('../db');
+
+exports.summary = async (req, res) => {
+  try {
+    const country = await pool.query('SELECT * FROM "Product" LIMIT 10');
+    res.status(200).json({
+      data: country.rows,
+    });
+  } catch (err) {
+    console.log(err.message);
+  }
 };
