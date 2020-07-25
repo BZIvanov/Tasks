a43d901e96658a9ab02737dfa2115ca1dd15cad8
BZIvanov <ivanov.biser@gmx.com>
2020-07-25 10:11:37 +0300

    optional date query

--- Dashboard/server/controllers/countries.js
+++ Dashboard/server/controllers/countries.js
@@ -1,10 +1,22 @@
 const pool = require('../db');
+const getDefaultDate = require('../utils/get-default-date');
 
 exports.summary = async (req, res) => {
+  const { flag } = req.params;
+  const { date = getDefaultDate() } = req.query;
+
   try {
-    const country = await pool.query('SELECT * FROM "Product" LIMIT 10');
+    const sources = await pool.query(
+      `SELECT DISTINCT "Source" FROM "Product" WHERE "Market" = $1 AND "ExtractionDate" = $2 ORDER BY "Source";`,
+      [flag.toUpperCase(), date]
+    );
+
+    const websites = sources.rows.map((source) => source['Source']);
+
     res.status(200).json({
-      data: country.rows,
+      country: flag,
+      websites: websites,
+      extractionDate: date,
     });
   } catch (err) {
     console.log(err.message);
---
+++ Dashboard/server/utils/get-default-date.js
@@ -0,0 +1,7 @@
+module.exports = () => {
+  const currentDate = new Date();
+  const year = currentDate.getFullYear();
+  const month = ('0' + (currentDate.getMonth() + 1)).slice(-2);
+  const day = currentDate.getDate();
+  return `${year}-${month}-${day}`;
+};
